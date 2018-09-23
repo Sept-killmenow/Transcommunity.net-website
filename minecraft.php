e9f8a117a82870abf76e7b6bb0fea750fab4de13
Sept <39297761+Sept-killmenow@users.noreply.github.com>
2018-09-23 22:58:50 +1200

    update

---
+++ minecraft.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<link rel="shortcut icon" href="imgs/favicon.png" type="image/x-icon">
+<link rel="icon" href="imgs/favicon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<title>TCH | Minecraft</title>
+<style>
+.dropdown-menu a:hover{
+ background:#23272a !important;
+}
+</style>
+</head>
+<body style="background:#2f3136;">
+	<div class="container-fluid" style="background:#23272a;">
+		<img src="imgs/tcm_logo.png" alt="Logo" class="mx-auto d-block col-md-3" draggable="false">
+	</div>
+	<div class="navbar-header shadow" style="background:#2a2c31;">
+		<nav class="nav navbar navbar-expand-sm navbar-dark justify-content-center" ">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Home</a>
+				</li>
+				<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle nav-dark active" href="#" id="navbardrop" data-toggle="dropdown">Minecraft</a>
+					<div class="dropdown-menu bg-dark ">
+						<a class="dropdown-item text-light active" href="#">About</a>
+						<a class="dropdown-item text-light" href="#">Announcements</a>
+						<a class="dropdown-item text-light" href="#">Bans</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Discord</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Patreon</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="col-md-8 mx-auto p-5 text-light shadow-sm">
+		<h3 class="text-align-center">Trans Community Minecraft</h3>
+		<hr>
+		<p></p>
+	</div>
+</body>
+</html>
